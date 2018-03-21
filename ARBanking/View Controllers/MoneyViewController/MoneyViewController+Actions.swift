f0cd56ec7f317c6acf4afc03963d8f26999f1dd6
Ricardo Salazar <risalba@gmail.com>
2018-03-21 23:17:38 +0100

    Push base project

---
+++ ARBanking/View Controllers/MoneyViewController/MoneyViewController+Actions.swift
@@ -0,0 +1,46 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+UI Actions for the main view controller.
+*/
+
+import UIKit
+import SceneKit
+
+extension MoneyViewController: UIGestureRecognizerDelegate {
+    
+    enum SegueIdentifier: String {
+        case showObjects
+    }
+    
+    // MARK: - Interface Actions
+    
+    /// Displays the `VirtualObjectSelectionViewController` from the `addObjectButton` or in response to a tap gesture in the `sceneView`.
+    
+    /// Determines if the tap gesture for presenting the `VirtualObjectSelectionViewController` should be used.
+    func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
+        return virtualObjectLoader.loadedObjects.isEmpty
+    }
+    
+    func gestureRecognizer(_: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith _: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    /// - Tag: restartExperience
+    func restartExperience() {
+        guard isRestartAvailable, !virtualObjectLoader.isLoading else { return }
+        isRestartAvailable = false
+
+        statusViewController.cancelAllScheduledMessages()
+
+        virtualObjectLoader.removeAllVirtualObjects()
+
+        resetTracking()
+
+        // Disable restart for a while in order to give the session time to restart.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.isRestartAvailable = true
+        }
+    }
+}
---
+++ ARBanking/View Controllers/MoneyViewController/MoneyViewController.swift
@@ -0,0 +1,272 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Main view controller for the AR experience.
+*/
+
+import ARKit
+import SceneKit
+import UIKit
+
+class MoneyViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet var sceneView: VirtualObjectARView!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+
+    // MARK: - UI Elements
+    
+    var focusSquare = FocusSquare()
+    
+    var moneyLoaded: Bool = false
+    
+    /// The view controller that displays the status and "restart experience" UI.
+    lazy var statusViewController: StatusViewController = {
+        return childViewControllers.lazy.flatMap({ $0 as? StatusViewController }).first!
+    }()
+	
+    // MARK: - ARKit Configuration Properties
+    
+    /// A type which manages gesture manipulation of virtual content in the scene.
+    lazy var virtualObjectInteraction = VirtualObjectInteraction(sceneView: sceneView)
+    
+    /// Coordinates the loading and unloading of reference nodes for virtual objects.
+    let virtualObjectLoader = VirtualObjectLoader()
+    
+    /// Marks if the AR experience is available for restart.
+    var isRestartAvailable = true
+    
+    /// A serial queue used to coordinate adding or removing nodes from the scene.
+    let updateQueue = DispatchQueue(label: "com.example.apple-samplecode.arkitexample.serialSceneKitQueue")
+    
+    var screenCenter: CGPoint {
+        let bounds = sceneView.bounds
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    /// Convenience accessor for the session owned by ARSCNView.
+    var session: ARSession {
+        return sceneView.session
+    }
+    
+    // MARK: - View Controller Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        focusSquare.delegate = self
+        
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+
+        // Set up scene content.
+        setupCamera()
+        sceneView.scene.rootNode.addChildNode(focusSquare)
+
+        /*
+         The `sceneView.automaticallyUpdatesLighting` option creates an
+         ambient light source and modulates its intensity. This sample app
+         instead modulates a global lighting environment map for use with
+         physically based materials, so disable automatic lighting.
+         */
+        sceneView.automaticallyUpdatesLighting = false
+        if let environmentMap = UIImage(named: "Models.scnassets/sharedImages/environment_blur.exr") {
+            sceneView.scene.lightingEnvironment.contents = environmentMap
+        }
+
+        // Hook up status view controller callback(s).
+        statusViewController.restartExperienceHandler = { [unowned self] in
+            self.restartExperience()
+        }
+        
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(loadMoney))
+        // Set the delegate to ensure this gesture is only used when there are no virtual objects in the scene.
+//        tapGesture.delegate = self
+//        sceneView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func loadMoney() {
+        loadModel(modelName: "5bill")
+    }
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		// Prevent the screen from being dimmed to avoid interuppting the AR experience.
+		UIApplication.shared.isIdleTimerDisabled = true
+
+        // Start the `ARSession`.
+        resetTracking()
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+
+        session.pause()
+	}
+
+    // MARK: - Scene content setup
+
+    func setupCamera() {
+        guard let camera = sceneView.pointOfView?.camera else {
+            fatalError("Expected a valid `pointOfView` from the scene.")
+        }
+
+        /*
+         Enable HDR camera settings for the most realistic appearance
+         with environmental lighting and physically based materials.
+         */
+        camera.wantsHDR = true
+        camera.exposureOffset = -1
+        camera.minimumExposure = -1
+        camera.maximumExposure = 3
+    }
+
+    // MARK: - Session management
+    
+    /// Creates a new AR configuration to run on the `session`.
+	func resetTracking() {
+		virtualObjectInteraction.selectedObject = nil
+		
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+		session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+
+        statusViewController.scheduleMessage("FIND A SURFACE TO PLACE AN OBJECT", inSeconds: 7.5, messageType: .planeEstimation)
+	}
+
+    // MARK: - Focus Square
+
+	func updateFocusSquare() {
+        let isObjectVisible = virtualObjectLoader.loadedObjects.contains { object in
+            return sceneView.isNode(object, insideFrustumOf: sceneView.pointOfView!)
+        }
+        
+        if isObjectVisible {
+            focusSquare.hide()
+        } else {
+            //focusSquare.unhide()
+            statusViewController.scheduleMessage("TRY MOVING LEFT OR RIGHT", inSeconds: 5.0, messageType: .focusSquare)
+        }
+		
+		if let result = self.sceneView.smartHitTest(screenCenter, allowedAlignments: [.horizontal]) {
+			updateQueue.async {
+				self.sceneView.scene.rootNode.addChildNode(self.focusSquare)
+				let camera = self.session.currentFrame?.camera
+				self.focusSquare.state = .detecting(hitTestResult: result, camera: camera)
+			}
+		} else {
+			updateQueue.async {
+				self.focusSquare.state = .initializing
+				self.sceneView.pointOfView?.addChildNode(self.focusSquare)
+			}
+			return
+		}
+		
+        statusViewController.cancelScheduledMessage(for: .focusSquare)
+	}
+    
+	// MARK: - Error handling
+    
+    func displayErrorMessage(title: String, message: String) {
+        // Blur the background.
+        blurView.isHidden = false
+        
+        // Present an alert informing about the error that has occurred.
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+            alertController.dismiss(animated: true, completion: nil)
+            self.blurView.isHidden = true
+            self.resetTracking()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
+// MARK: Place Objects
+
+extension MoneyViewController {
+    /**
+     Adds the specified virtual object to the scene, placed using
+     the focus square's estimate of the world-space position
+     currently corresponding to the center of the screen.
+     
+     - Tag: PlaceVirtualObject
+     */
+    func placeVirtualObject(_ virtualObject: VirtualObject) {
+        guard let cameraTransform = session.currentFrame?.camera.transform,
+            let focusSquareAlignment = focusSquare.recentFocusSquareAlignments.last,
+            focusSquare.state != .initializing else {
+                statusViewController.showMessage("CANNOT PLACE OBJECT\nTry moving left or right.")
+                return
+        }
+        
+        // The focus square transform may contain a scale component, so reset scale to 1
+        let focusSquareScaleInverse = 1.0 / focusSquare.simdScale.x
+        let scaleMatrix = float4x4(uniformScale: focusSquareScaleInverse)
+        let focusSquareTransformWithoutScale = focusSquare.simdWorldTransform * scaleMatrix
+        
+        virtualObjectInteraction.selectedObject = virtualObject
+        virtualObject.setTransform(focusSquareTransformWithoutScale,
+                                   relativeTo: cameraTransform,
+                                   smoothMovement: false,
+                                   alignment: focusSquareAlignment,
+                                   allowAnimation: false)
+        
+        updateQueue.async {
+            self.sceneView.scene.rootNode.addChildNode(virtualObject)
+            self.sceneView.addOrUpdateAnchor(for: virtualObject)
+        }
+    }
+    
+    // MARK: - VirtualObjectSelectionViewControllerDelegate
+    
+    func loadModel(modelName: String) {
+        if let filePath = Bundle.main.path(forResource: modelName, ofType: "scn", inDirectory: "Models.scnassets/\(modelName)") {
+            let referenceURL = NSURL(fileURLWithPath: filePath)
+            if let referenceNode = VirtualObject(url: referenceURL as URL) {
+                virtualObjectLoader.loadVirtualObject(referenceNode, loadedHandler: { [unowned self] loadedObject in
+                    DispatchQueue.main.async {
+                        self.hideObjectLoadingUI()
+                        self.placeVirtualObject(loadedObject)
+                        self.moneyLoaded = true
+                    }
+                })
+            }
+        }
+        DispatchQueue.main.async {
+            self.displayObjectLoadingUI()
+        }
+    }
+    
+    // MARK: Object Loading UI
+    
+    func displayObjectLoadingUI() {
+        // Show progress indicator.
+        spinner.startAnimating()
+        isRestartAvailable = false
+    }
+    
+    func hideObjectLoadingUI() {
+        // Hide progress indicator.
+        spinner.stopAnimating()
+        isRestartAvailable = true
+    }
+}
+
+extension MoneyViewController: FocusSquareDelegate {
+    func foundPlane() {
+        if !moneyLoaded {
+            loadMoney()
+            moneyLoaded = true
+        }
+    }
+}
+
